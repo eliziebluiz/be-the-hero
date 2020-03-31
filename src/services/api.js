7bb6173e23c4037db1cfc1dca162885e08b9078c
Elizieb <elizieb.l3@gmail.com>
2020-03-30 22:45:25 -0300

    Alterando baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const back = 'be-the-hero2.herokuapp.com/'
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.119:3333'
+  baseURL: `http://192.168.0.119:${back}`
 });
 
 export default api;
